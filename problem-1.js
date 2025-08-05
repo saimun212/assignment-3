134644c0f682ef2b1d1088a1a9ffc0211e998dd2
saimun212 <saimunislam00402@gmail.com>
2025-08-05 11:14:33 +0600

    add our javascript conde and output

--- problem-1.js
+++ problem-1.js
@@ -1,4 +1,13 @@
 /** Problem -01 ( Divide the Asset ) */
 var area = 800;
 //write your code here
-console.log('')
+
+/*=================Result-javascript-problem-1-Answer================== */
+var area = 100 / 2 ;
+console.log(area);
+
+var area = 15 / 2;
+console.log(area);
+
+var area = 2060 / 2;
+console.log(area);
