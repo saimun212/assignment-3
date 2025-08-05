1f5ebb6251eb2e09f2befeb8623564edd149b90f
saimun212 <saimunislam00402@gmail.com>
2025-08-05 13:17:25 +0600

    add our new java code and commit problem-5 file and output

--- problem-5.js
+++ problem-5.js
@@ -1,1 +1,20 @@
 /**===========starting---question-----part============== */
+/** Problem 05 - ( PH Email Generator )  */
+var student= { name: "jhankar" , roll: 1014 ,department: "cse" };
+//write your code here
+
+/*=====starting--answer-part-=========== */
+/*============> 1st-parson-id   <=========== */
+var email = student.name + student.roll + "" + student.department + "@ph.ac.bd";
+console.log(email);
+
+/*============> 2nd-parson-id   <=========== */
+var student= { name: "monu" , roll:99 ,department: "eee" };
+var email = student.name + student.roll + "" + student.department + "@ph.ac.bd";
+console.log(email);
+
+
+/*============> 3rd-parson-id   <=========== */
+var student= { name: "mewo" , roll:96 ,department: "cse" };
+var email = student.name + student.roll + "" + student.department + "@ph.ac.bd";
+console.log(email);
