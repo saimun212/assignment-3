cc3971a1f2471862c8f2c371bfe73b1f329a715b
saimun212 <saimunislam00402@gmail.com>
2025-08-05 12:20:17 +0600

    add our third problem

--- problem-3.js
+++ problem-3.js
@@ -1,4 +1,26 @@
 /*==================problem-3-question==================== */
 /** Problem -03 ( Medicine Planner ) */
-var lastDay = 11 ;
+var lastDay = 4 ;
 //write your code here
+
+/*==================Start---Answer-parts=================== */
+/*=============first ----- output=========*/
+for(var day = 1; day <=lastDay; day++){
+    if (day % 3 ===0){
+        console.log(day + " medicine");
+    }
+    else{
+        console.log(day +  " rest");
+    }
+}
+
+/*===============second---output================ */
+var everyDay = 6;
+for(var day = 1; day <=everyDay; day++){
+    if (day % 3 ===0){
+        console.log(day + " medicine");
+    }
+    else{
+        console.log(day +  " rest");
+    }
+}
